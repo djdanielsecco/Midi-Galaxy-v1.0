1d16b92659a6ee2a8f17a337a99b83bbf20b6571
djdanielsecco <djdanielsecco@gmail.com>
2018-01-14 22:32:27 -0200

    wewew

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -12,78 +12,76 @@ var precacheFiles = [
     ];
 
 
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-  evt.waitUntil(precache().then(function() {
-    console.log('[ServiceWorker] Skip waiting on install');
-      return self.skipWaiting();
+self.addEventListener('install', function (evt) {
+	console.log('The service worker is being installed.');
+	evt.waitUntil(precache().then(function () {
+		console.log('[ServiceWorker] Skip waiting on install');
+		return self.skipWaiting();
 
-  })
-  );
+	}));
 });
 
 
- self.addEventListener('fetch', function(event) {
-   event.respondWith(
-     caches.match(event.request)
-       .then(function(response) {
-         if (response) {
-           return response;
-         } else {
-           return fetch(event.request)
-             .then(function(res) {
-               return caches.open(CACHE_DYNAMIC_NAME)
-                 .then(function(cache) {
-                   cache.put(event.request.url, res.clone());
-                   return res;
-                 })
-             })
-             .catch(function(err) {
-               return caches.open(CACHE_STATIC_NAME)
-                 .then(function(cache) {
-                   return cache.match('/offline.html');
-                 });
-             });
-         }
-       })
-   );
- });
-
- self.addEventListener('fetch', function(event) {
-   event.respondWith(
-     fetch(event.request)
-       .then(function(res) {
-         return caches.open(CACHE_DYNAMIC_NAME)
-                 .then(function(cache) {
-                   cache.put(event.request.url, res.clone());
-                   return res;
-                 })
-       })
-       .catch(function(err) {
-         return caches.match(event.request);
-       })
-   );
- });
-
- //Cache-only
- self.addEventListener('fetch', function (event) {
-   event.respondWith(
-     caches.match(event.request)
-   );
- });
-
- //Network-only
- self.addEventListener('fetch', function (event) {
-   event.respondWith(
-     fetch(event.request)
-   );
- });
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function (response) {
+			if (response) {
+				return response;
+			} else {
+				return fetch(event.request)
+					.then(function (res) {
+						return caches.open(CACHE_DYNAMIC_NAME)
+							.then(function (cache) {
+								cache.put(event.request.url, res.clone());
+								return res;
+							})
+					})
+					.catch(function (err) {
+						return caches.open(CACHE_STATIC_NAME)
+							.then(function (cache) {
+								return cache.match('/offline.html');
+							});
+					});
+			}
+		})
+	);
 });
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		fetch(event.request)
+		.then(function (res) {
+			return caches.open(CACHE_DYNAMIC_NAME)
+				.then(function (cache) {
+					cache.put(event.request.url, res.clone());
+					return res;
+				})
+		})
+		.catch(function (err) {
+			return caches.match(event.request);
+		})
+	);
+});
+
+//Cache-only
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request)
+	);
+});
+
+//Network-only
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		fetch(event.request)
+	);
+});
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
+	);
 });
